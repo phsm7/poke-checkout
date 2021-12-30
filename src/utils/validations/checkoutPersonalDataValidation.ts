14b77367fe9b25b3c1a6262b0c465701a945150b
Pedro Machado <pedrohsmachado77@gmail.com>
2021-12-30 19:39:44 -0300

    feat: add validations

--- src/utils/validations/checkoutPersonalDataValidation.ts
+++ src/utils/validations/checkoutPersonalDataValidation.ts
@@ -1,3 +1,5 @@
+import desmaskPhone from 'utils/masks/desmaskPhone';
+import desmaskCpf from 'utils/masks/desmaskCpf';
 import * as Yup from 'yup';
 import { getValidationErrors } from './getValidationErrors';
 
@@ -6,17 +8,24 @@ type Values = {
 };
 
 export const checkoutPersonalDataValidation = async (values: Values) => {
+  const desmaskedValues = {
+    ...values,
+    cpf: values.cpf ? desmaskCpf(values.cpf) : values.cpf,
+    phone: values.phone ? desmaskPhone(values.phone) : values.phone,
+  };
   try {
     const schema = Yup.object().shape({
-      cpf: Yup.string().required('Campo obrigatório'),
-      phone: Yup.number().required('Campo obrigatório'),
-      email: Yup.string().email().required('Campo obrigatório'),
+      cpf: Yup.string().required('Campo obrigatório').min(11, 'Mínimo 11'),
+      phone: Yup.string().required('Campo obrigatório').min(11, 'Mínimo 11'),
+      email: Yup.string()
+        .email('Informe um email válido')
+        .required('Campo obrigatório'),
       rg: Yup.string().required('Campo obrigatório'),
       birthday: Yup.string().required('Campo obrigatório'),
       motherName: Yup.string().required('Campo obrigatório'),
     });
 
-    await schema.validate(values, { abortEarly: false });
+    await schema.validate(desmaskedValues, { abortEarly: false });
   } catch (err) {
     if (err instanceof Yup.ValidationError) {
       return getValidationErrors(err);
